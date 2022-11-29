2ae12891880f6b0af44cbb4599e21289e1c12710
IvanKahinga <ikahinga@gmail.com>
2022-11-29 12:24:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// Arrow function that divides numbers
+
+const divide = (num1, num2) => 2000 / 100
+
+console.log( divide())
+
+// Arrow function that squares numbers
+const square = x => 2**2
+
+//Arrow function that adds numbers
+
+let a = 3
+let b = 4
+const add = (a, b) => a + b
